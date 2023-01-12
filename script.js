9f16483620893645e164ccf3ae5afb695ee8ae32
Shaeen Singh <shaeendev@gmail.com>
2023-01-12 20:26:38 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let docTitle = document.title;
+
+window.addEventListener("blur", () => {
+  document.title = "Come back :(";
+});
+
+window.addEventListener("focus", () => {
+  document.title = docTitle;
+});
